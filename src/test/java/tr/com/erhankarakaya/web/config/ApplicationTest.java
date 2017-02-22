7c261ee123cd47615ee52d17deb32166e5bc4b2c
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-22 17:01:50 +0300

    Added test for app main class

---
+++ src/test/java/tr/com/erhankarakaya/web/config/ApplicationTest.java
@@ -0,0 +1,19 @@
+package tr.com.erhankarakaya.web.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by erhan.karakaya on 2/22/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
