89690582833e7c228edd6bfa3c6f086620803bb1
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-22 17:01:20 +0300

    Added app main class

---
+++ src/main/java/tr/com/erhankarakaya/web/config/Application.java
@@ -0,0 +1,18 @@
+package tr.com.erhankarakaya.web.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by erhan.karakaya on 2/22/2017.
+ *
+ * Spring Boot start-up class
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
