595f4704aff5ace7079a5ab0a0366a9e12e20007
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-24 10:23:10 +0300

    Added base dtos

---
+++ src/main/java/tr/com/erhankarakaya/web/bll/dto/BaseDto.java
@@ -0,0 +1,9 @@
+package tr.com.erhankarakaya.web.bll.dto;
+
+import java.io.Serializable;
+
+/**
+ * Created by erhan.karakaya on 2/24/2017.
+ */
+public class BaseDto implements Dto, Serializable {
+}
---
+++ src/main/java/tr/com/erhankarakaya/web/bll/dto/Dto.java
@@ -0,0 +1,7 @@
+package tr.com.erhankarakaya.web.bll.dto;
+
+/**
+ * Created by erhan.karakaya on 2/24/2017.
+ */
+public interface Dto {
+}
