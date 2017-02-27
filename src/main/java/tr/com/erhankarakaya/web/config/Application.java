f2c617cba78f02fc01a301fc158852db4ac0c6e7
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-27 14:03:47 +0300

    Added localeResolver bean

--- src/main/java/tr/com/erhankarakaya/web/config/Application.java
+++ src/main/java/tr/com/erhankarakaya/web/config/Application.java
@@ -2,6 +2,14 @@ package tr.com.erhankarakaya.web.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Created by erhan.karakaya on 2/22/2017.
@@ -9,10 +17,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Spring Boot start-up class
  */
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+    sessionLocaleResolver.setDefaultLocale(new Locale("tr"));
+    return sessionLocaleResolver;
+  }
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
 }
