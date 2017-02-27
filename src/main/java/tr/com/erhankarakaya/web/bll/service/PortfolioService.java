e4557b43ee67437d85cd4013a6eca268efb94269
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-27 14:06:23 +0300

    Added portfolioService

---
+++ src/main/java/tr/com/erhankarakaya/web/bll/service/PortfolioService.java
@@ -0,0 +1,15 @@
+package tr.com.erhankarakaya.web.bll.service;
+
+import org.springframework.data.domain.Pageable;
+import tr.com.erhankarakaya.web.bll.dto.PortfolioDto;
+import tr.com.erhankarakaya.web.common.crudresult.CrudResult;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by erhan.karakaya on 2/24/2017.
+ */
+@Transactional
+public interface PortfolioService {
+  CrudResult<PortfolioDto> findAll(Pageable pageable);
+}
---
+++ src/main/java/tr/com/erhankarakaya/web/bll/service/PortfolioServiceImpl.java
@@ -0,0 +1,46 @@
+package tr.com.erhankarakaya.web.bll.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import tr.com.erhankarakaya.web.bll.dto.PortfolioDto;
+import tr.com.erhankarakaya.web.bll.mapper.PortfolioMapper;
+import tr.com.erhankarakaya.web.common.crudresult.CrudResult;
+import tr.com.erhankarakaya.web.common.enums.ResultEnum;
+import tr.com.erhankarakaya.web.dal.entity.Portfolio;
+import tr.com.erhankarakaya.web.dal.repository.PortfolioRepository;
+
+import java.util.Locale;
+
+
+/**
+ * Created by erhan.karakaya on 2/27/2017.
+ */
+@Service
+public class PortfolioServiceImpl implements PortfolioService {
+
+  @Autowired
+  protected MessageSource messageSource;
+
+  @Autowired
+  private PortfolioRepository portfolioRepository;
+
+  @Override
+  public CrudResult<PortfolioDto> findAll(Pageable pageable) {
+    Assert.notNull(messageSource);
+    Page<Portfolio> portfolioPage = portfolioRepository.findAll(pageable);
+
+    Page<PortfolioDto> portfolioDtoPage = PortfolioMapper.mapPageEntityToPageDto(portfolioPage,pageable);
+
+    CrudResult<PortfolioDto> crudResult = new CrudResult<>();
+    //crudResult.setMessage(messageSource.getMessage("crudresult.success", null, new Locale("tr")));
+    crudResult.setReturnDtos(portfolioDtoPage);
+    crudResult.setResult(ResultEnum.SUCCESS);
+
+    return crudResult;
+  }
+}
