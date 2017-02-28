31ec31c7e5cf4a4c7a0caedcbf8b508f5c12f4b9
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-28 09:18:50 +0300

    Added test for findAll method

--- src/test/java/tr/com/erhankarakaya/web/bll/service/PortfolioServiceTest.java
+++ src/test/java/tr/com/erhankarakaya/web/bll/service/PortfolioServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import tr.com.erhankarakaya.web.bll.dto.PortfolioDto;
 import tr.com.erhankarakaya.web.common.crudresult.CrudResult;
+import tr.com.erhankarakaya.web.common.enums.LanguageEnum;
 import tr.com.erhankarakaya.web.dal.entity.Portfolio;
 import tr.com.erhankarakaya.web.dal.repository.PortfolioRepository;
 
@@ -34,6 +35,12 @@ import static org.mockito.Mockito.when;
 @ContextConfiguration(classes = {PortfolioTestConfig.class, PortfolioServiceImpl.class})
 public class PortfolioServiceTest {
 
+  private static final Integer ID = 20;
+  private static final String TITLE = "PORTFOLIO 1";
+  private static final String DESCRIPTION = "Lorem Ipsum Portfolio Description.";
+  private static final String IMAGE_FILE_NAME = "portfolio1.jpg";
+  private static final LanguageEnum LANGUAGE = LanguageEnum.TR;
+  private static final Integer ORDERING_NUMBER = 1;
 
   @Autowired
   private PortfolioRepository portfolioRepository;
@@ -42,16 +49,36 @@ public class PortfolioServiceTest {
   private PortfolioService portfolioService;
 
   @Test
-  public void whenNoEntryFoundReturnEmptyResult() {
+  public void whenNoEntryFound_ShouldReturnEmptyResult() {
     List<Portfolio> portfolios = new ArrayList<>();
     Page<Portfolio> portfolioPage = new PageImpl<Portfolio>(portfolios);
 
     when(portfolioRepository.findAll(any(Pageable.class))).thenReturn(portfolioPage);
 
-    CrudResult<PortfolioDto> crudResult = portfolioService.findAll(new PageRequest(1,10));
+    CrudResult<PortfolioDto> crudResult = portfolioService.findAll(new PageRequest(0, 10));
 
-    assertEquals(0,crudResult.getReturnDtos().get().getTotalElements());
+    assertEquals(0, crudResult.getReturnDtos().get().getTotalElements());
   }
 
+  @Test
+  public void whenOneEntryFound_ShouldReturnOneItemContainingList() {
+    List<Portfolio> portfolios = new ArrayList<>();
+    Portfolio.PortfolioBuilder portfolioBuilder = new Portfolio.PortfolioBuilder();
+    Portfolio portfolio1 = portfolioBuilder.id(ID)
+        .title(TITLE)
+        .description(DESCRIPTION)
+        .imageFileName(IMAGE_FILE_NAME)
+        .languageId(LANGUAGE.getId())
+        .orderingNumber(ORDERING_NUMBER)
+        .build();
+    portfolios.add(portfolio1);
+    Page<Portfolio> portfolioPage = new PageImpl<Portfolio>(portfolios);
+
+    when(portfolioRepository.findAll(any(Pageable.class))).thenReturn(portfolioPage);
+
+    CrudResult<PortfolioDto> crudResult = portfolioService.findAll(new PageRequest(0, 10));
+
+    assertEquals(1, crudResult.getReturnDtos().get().getTotalElements());
+  }
 
 }
