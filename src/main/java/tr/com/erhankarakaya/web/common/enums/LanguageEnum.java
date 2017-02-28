11b9e830b99aaf47e38920797508d3dd398bbcb4
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-28 16:39:46 +0300

    Added locale to language conversion

--- src/main/java/tr/com/erhankarakaya/web/common/enums/LanguageEnum.java
+++ src/main/java/tr/com/erhankarakaya/web/common/enums/LanguageEnum.java
@@ -1,5 +1,9 @@
 package tr.com.erhankarakaya.web.common.enums;
 
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
+
 /**
  * Created by erhan.karakaya on 2/24/2017.
  */
@@ -14,5 +18,20 @@ public enum LanguageEnum {
 
   public Integer getId() {
     return id;
+  }
+
+  public static LanguageEnum getLanguageByLocale(Locale locale) {
+    switch (locale.getLanguage().toLowerCase()) {
+      case "en":
+        return EN;
+      case "tr":
+      default:
+        return TR;
+    }
+  }
+
+  public static LanguageEnum getCurrentLanguage() {
+    Locale locale = LocaleContextHolder.getLocale();
+    return getLanguageByLocale(locale);
   }
 }
