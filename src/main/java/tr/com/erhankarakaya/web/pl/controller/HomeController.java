6155c024f5248c66f518e01f7efd2185d6befa93
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-28 16:42:01 +0300

    Fixed HomeController to get portfolio by languageId

--- src/main/java/tr/com/erhankarakaya/web/pl/controller/HomeController.java
+++ src/main/java/tr/com/erhankarakaya/web/pl/controller/HomeController.java
@@ -1,6 +1,7 @@
 package tr.com.erhankarakaya.web.pl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import tr.com.erhankarakaya.web.bll.dto.PortfolioDto;
 import tr.com.erhankarakaya.web.bll.service.PortfolioService;
 import tr.com.erhankarakaya.web.common.crudresult.CrudResult;
+import tr.com.erhankarakaya.web.common.enums.LanguageEnum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by erhan.karakaya on 2/28/2017.
@@ -25,7 +28,7 @@ public class HomeController {
   @RequestMapping({"/", "/index"})
   public String index(Model model) {
 
-    CrudResult<PortfolioDto> crudResult = portfolioService.findAll();
+    CrudResult<PortfolioDto> crudResult = portfolioService.findAllByLanguageId(LanguageEnum.getCurrentLanguage());
 
     List<PortfolioDto> portfolioDtos = crudResult.getReturnDtos().orElse(new ArrayList<>());
 
