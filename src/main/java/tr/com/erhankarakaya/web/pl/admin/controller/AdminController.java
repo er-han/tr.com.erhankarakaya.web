e81220a812429dd5f104f9497178fd3c4c2f1365
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-28 17:19:28 +0300

    Added AdminController

---
+++ src/main/java/tr/com/erhankarakaya/web/pl/admin/controller/AdminController.java
@@ -0,0 +1,12 @@
+package tr.com.erhankarakaya.web.pl.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by erhan.karakaya on 2/28/2017.
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+}
