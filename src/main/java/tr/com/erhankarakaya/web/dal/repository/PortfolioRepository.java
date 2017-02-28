7b4b2a3605cb6736d520f5132dc947a51e73b250
Erhan KARAKAYA <erhan277@gmail.com>
2017-02-28 16:40:34 +0300

    Added findAllByLanguageId method into PortfolioRepository

--- src/main/java/tr/com/erhankarakaya/web/dal/repository/PortfolioRepository.java
+++ src/main/java/tr/com/erhankarakaya/web/dal/repository/PortfolioRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
 import tr.com.erhankarakaya.web.dal.entity.Portfolio;
 
+import java.util.List;
+
 /**
  * Created by erhan.karakaya on 2/23/2017.
  */
 @Repository
 public interface PortfolioRepository extends BaseRepository<Portfolio, Integer> {
+  List<Portfolio> findAllByLanguageId(Integer languageId);
 }
