151f5c1a91d65f65ae9856fcf3b2a4508c5a5bcb
Erhan KARAKAYA <erhan277@gmail.com>
2017-03-01 09:52:54 +0300

    Added AdminController

--- src/main/java/tr/com/erhankarakaya/web/pl/admin/controller/AdminController.java
+++ src/main/java/tr/com/erhankarakaya/web/pl/admin/controller/AdminController.java
@@ -1,6 +1,7 @@
 package tr.com.erhankarakaya.web.pl.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,4 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+  @GetMapping({"", "/index"})
+  public String index() {
+    return "admin/index";
+  }
 }
