dda59294cfe11d3a2cd43bc26e2fa62b324b2316
Erhan KARAKAYA <erhan277@gmail.com>
2017-03-03 17:18:32 +0300

    Fixed portfolio edit action method

--- src/main/java/tr/com/erhankarakaya/web/pl/admin/controller/PortfolioController.java
+++ src/main/java/tr/com/erhankarakaya/web/pl/admin/controller/PortfolioController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tr.com.erhankarakaya.web.bll.dto.PortfolioDto;
 import tr.com.erhankarakaya.web.bll.dto.PortfolioListDto;
 import tr.com.erhankarakaya.web.bll.service.PortfolioService;
 import tr.com.erhankarakaya.web.common.crudresult.CrudResult;
@@ -22,12 +25,28 @@ public class PortfolioController extends BaseAdminController {
   private PortfolioService portfolioService;
 
   @GetMapping("/portfolio/list")
-  public String listPortfolios(Model model) {
+  public String getListOfPortfolios(Model model) {
     CrudResult<PortfolioListDto> crudResult = portfolioService.findAllForList();
     if (crudResult.isSuccess()) {
       model.addAttribute("portfolios", crudResult.getReturnDtos().orElse(new ArrayList<>()));
     }
 
     return "/admin/portfolio-list";
+  }
+
+
+  @GetMapping("/portfolio/edit")
+  public String getDetailOfPortfolio(@RequestParam(value = "id", required = false) Integer id, Model model) {
+    CrudResult<PortfolioDto> crudResult = portfolioService.findById(id);
+
+    PortfolioDto portfolioDto;
+    if (crudResult.isSuccess() && crudResult.getReturnDto().isPresent()) {
+      portfolioDto = crudResult.getReturnDto().get();
+    } else {
+      portfolioDto = new PortfolioDto.PortfolioDtoBuilder().build();
+    }
+    model.addAttribute("portfolio", portfolioDto );
+
+    return "/admin/portfolio-edit";
   }
 }
