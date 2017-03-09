9c22f01dd33a0da6ceeacb23bb9d7346e6571c24
Erhan KARAKAYA <erhan277@gmail.com>
2017-03-09 15:36:23 +0300

    Added new val into ResultEnum: NA

--- src/main/java/tr/com/erhankarakaya/web/common/enums/ResultEnum.java
+++ src/main/java/tr/com/erhankarakaya/web/common/enums/ResultEnum.java
@@ -4,7 +4,7 @@ package tr.com.erhankarakaya.web.common.enums;
  * Created by erhan.karakaya on 2/24/2017.
  */
 public enum ResultEnum {
-  SUCCESS, WARNING, ERROR;
+  SUCCESS, WARNING, ERROR, NA;
 
   public boolean isSuccess() {
     return this == ResultEnum.SUCCESS;
